d6613a131c5f8769828ea97fc80a1ebac0f25d8f
Ronald Marcel <ronald.marcel.mr@gmail.com>
2026-06-23 20:45:09 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
-    API_URL: 'http://192.168.0.60:13334',
+    API_URL: 'truenas.local:13334',
     STRATUM_URL: 'truenas.local:13333',
-    SECURE_STRATUM_URL: 'truenas.local:4333',
-    STRATUM_V2_URL: 'truenas.local:23330',
-    PPLNS_STRATUM_URL: 'truenas.local:13333',
-    PPLNS_SECURE_STRATUM_URL: 'truenas.local:14333',
-    PPLNS_STRATUM_V2_URL: 'truenas.local:23331',
-    PPLNS_DATUM_URL: 'truenas.local:23336',
+    SECURE_STRATUM_URL: '',
+    STRATUM_V2_URL: '',
+    PPLNS_STRATUM_URL: '',
+    PPLNS_SECURE_STRATUM_URL: '',
+    PPLNS_STRATUM_V2_URL: '',
+    PPLNS_DATUM_URL: '',
 };
